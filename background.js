91e056180a0ab33526a8eb844e6c8ddac05f437a
MD ARIFUL ISLAM <137490686+udcpanel@users.noreply.github.com>
2024-12-21 12:18:14 +0600

    Update background.js for Right Click Enable's Background Data
    Update background.js for Right Click Enable's Background Data

--- background.js
+++ background.js
@@ -2,3 +2,25 @@
 browser.browserAction.onClicked.addListener(() => {
   browser.tabs.create({ url: 'popup.html' });
 });
+
+// enabling right-click start//
+chrome.contextMenus.create({
+  id: "enableRightClick",
+  title: "Enable Right Click",
+  contexts: ["all"]
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "enableRightClick") {
+    // No action needed, just enabling right-click
+  }
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status === "complete") {
+      chrome.tabs.executeScript(tabId, { file: "content.js" });
+    }
+  });
+});
+// enabling right-click end//
