04ac0dd1acf5523cac61d4a07565b210aa89a07b
MD ARIFUL ISLAM <137490686+udcpanel@users.noreply.github.com>
2025-01-18 18:59:36 +0600

    সনদ যাচায়কারী সংযুক্ত

---
+++ scripts/certificate_verifier.js
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// Get the 4th table on the page
+var table = document.getElementsByTagName("table")[3];
+
+// Get the first td element of the 2nd table
+var td = table.getElementsByTagName("td")[1];
+
+
+ td.innerHTML = '<td width="33%" align="center" valign="top" nowrap="nowrap"> যাচায়কারী <br> <br> <br> <br> <strong> ইউপি সদস্য </strong><br> ৩নং শিকলবাহা ইউনিয়ন পরিষদ<br> কর্ণফুলী উপজেলা, চট্টগ্রাম </td>';
